f61e7346aa05126670807274ae2dff608fd5128b
yamadaippei <60645937+yamadaippei@users.noreply.github.com>
2020-07-06 20:02:03 +0900

    indicate toppage
    # what
    トップページの表示ができるように設定
    (controller、model、viewのindex)
    # why
    ここから作業を分けるため一旦まとめる。

--- db/schema.rb
+++ db/schema.rb
@@ -10,6 +10,11 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 0) do
+ActiveRecord::Schema.define(version: 2020_07_06_105640) do
+
+  create_table "schedules", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
 end
